91f41744ad60af43d0671675d24d372cbe7b28b5
Juan Pablo Ramos Velez <juanp.ramos@udea.edu.co>
2020-10-17 17:00:00 -0500

    Opciones para saber que parametro poner a las variables y que se muestre lo que el usuario quiere.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,37 @@ using namespace std;
 
 int main()
 {
+    int opcion=0;
+    bool impactoCañonO=false, impactoCañonD=false;
     float tO, tD, t0O=0, t0D=2.5, radioD =0.025, radioO =0.05, Ho, HD, distancia, anguloO, anguloD;
     float yO, vyO, g=9.8, xO, vxO, yD, vyD, xD, vxD, v0O, v0D;
+
+    // Opciones
+    cout <<" Elija una de las opciones: "<<endl;
+    cout <<"1. Generar disparos (al menos tres) ofensivos que comprometan la integridad del cañón defensivo."<<endl;
+    cout <<"2. Generar disparos (al menos tres) defensivos que comprometan la integridad del cañón ofensivo."<<endl;
+    cout <<"3. Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el cañón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido."<<endl;
+    cout <<"4. Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos."<<endl;
+    cin >> opcion;
+
+    if (opcion == 1){
+
+    }
+    else if (opcion == 2){
+
+    }
+    else if (opcion == 3){
+
+    }
+    else if (opcion == 4){
+
+    }
+    else {
+        cout<< "opcion incorrecta. ";
+        return 0;
+    }
+
+
 
     //Ecuaciones.
 
